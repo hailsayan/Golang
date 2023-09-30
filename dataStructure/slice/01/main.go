f6783fd1fb17d533acba89dc89737459113b8a52
Psyon <sayan.h2020@gmail.com>
2023-09-30 21:39:54 +0330

    add make silce

--- dataStructure/slice/01/main.go
+++ dataStructure/slice/01/main.go
@@ -2,27 +2,11 @@ package main
 
 import "fmt"
 
-// s := arr[start:end]
+// var s []T
 
 func main() {
-	var colors = [5]string{
-		"Red",
-		"Blue",
-		"Green",
-		"Pink",
-		"Purple",
-	}
-	s1 := colors[1:3] // Selected Indices : 1,2
+	var s []string
 
-	s2 := colors[:3] // Selected Indices : 0,1,2
-
-	s3 := colors[2:] // Selected Indices : 2,3,4
-
-	s4 := colors[:] // Selected Indices : 0,1,2,3,4
-
-	fmt.Println("Array:", colors)
-	fmt.Println("Slice 1:", s1)
-	fmt.Println("Slice 2:", s2)
-	fmt.Println("Slice 3:", s3)
-	fmt.Println("Slice 4:", s4)
+	fmt.Println(s)
+	fmt.Println(s == nil)
 }
---
+++ dataStructure/slice/03/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// make([]T,len)
+
+func main() {
+	s1 := make([]string, 0)
+	fmt.Println(s1)
+
+	s2 := make([]int, 3)
+	fmt.Println(s2)
+}
