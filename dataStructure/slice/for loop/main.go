b48d0561ff07d618d72a1e4e7aba773d7c35e134
Psyon <sayan.h2020@gmail.com>
2023-10-03 13:48:17 +0330

    add methods for slice

--- dataStructure/slice/for loop/main.go
+++ dataStructure/slice/for loop/main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 func main() {
 	letters := []string{"a", "b", "c", "d"}
+	// method 1
 	for _, letter := range letters {
-		fmt.Printf("%s")
+		fmt.Printf("%s\t", letter)
+	}
+	fmt.Printf("\n")
+	// method2
+	for i := 0; i < len(letters); i++ {
+		fmt.Printf("%s\t", letters[i])
 	}
 }
