da61e0dcce9b54e69724f12a961ffc60d3a52f4f
Psyon <sayan.h2020@gmail.com>
2023-10-10 22:24:56 +0330

    add a doc to slice/combOdd&Even

--- dataStructure/slice/combOdd&Even/main.go
+++ dataStructure/slice/combOdd&Even/main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
+// ... is used to unpack a slice and pass its individual elements to a variadic function or append its elements to another slice.
 func main() {
 	even := []int{2, 4, 6}
 	odd := []int{1, 3, 5}
-	numbers := append(odd, even...) // ...
-
+	numbers := append(odd, even...)
 	fmt.Println(numbers)
 }
