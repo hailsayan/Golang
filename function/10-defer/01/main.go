cea437e8134e363594a93197c128506eff213cf1
Psyon <sayan.h2020@gmail.com>
2023-12-13 21:38:15 +0330

    edit defer/01

--- function/10-defer/01/main.go
+++ function/10-defer/01/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("second")
 	defer fmt.Println("forth")
 	defer fmt.Println("third")
+	defer fmt.Println("second")
 
 	fmt.Println("first")
 }
