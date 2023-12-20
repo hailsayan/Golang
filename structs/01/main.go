a6b8dba639fcb9e04d3226e421a0f9a7f7cd1e11
Psyon <sayan.h2020@gmail.com>
2023-12-20 19:02:21 +0330

    edit structs/01

--- structs/01/main.go
+++ structs/01/main.go
@@ -1,3 +1,14 @@
 package main
 
-type Student struct{}
+import "fmt"
+
+type Student struct {
+	firstName string
+	lastName  string
+	age       int
+}
+
+func main() {
+	var s1 Student
+	fmt.Printf("%+v", s1)
+}
