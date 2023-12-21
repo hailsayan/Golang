95e22b71bf19cca4fb48e4010c4a340f887e6d43
Psyon <sayan.h2020@gmail.com>
2023-12-21 13:08:58 +0330

    edit struct/01

--- structs/01/main.go
+++ structs/01/main.go
@@ -10,5 +10,18 @@ type Student struct {
 
 func main() {
 	var s1 Student
-	fmt.Printf("%+v", s1)
+	fmt.Printf("%+v\n", s1)
+
+	var s2 Student = Student{
+		firstName: "John",
+		lastName:  "Doe",
+		age:       25,
+	}
+	fmt.Printf("%+v\n", s2)
+
+	var s3 = Student{
+		firstName: "Psyon",
+		age:       20,
+	}
+	fmt.Printf("%+v\n", s3)
 }
