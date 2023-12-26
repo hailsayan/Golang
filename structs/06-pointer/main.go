74584c57874c4ab75d1a3c15dcd316e4013b0341
Psyon <sayan.h2020@gmail.com>
2023-12-26 20:53:34 +0330

    add increamentAge to struct/06

--- structs/06-pointer/main.go
+++ structs/06-pointer/main.go
@@ -8,10 +8,17 @@ type Person struct {
 	Age       int
 }
 
+func increamentAge(p *Person) {
+	p.Age++
+}
+
 func main() {
 	p := Person{FirstName: "John", LastName: "Doe", Age: 25}
 	ptr := &p
 	fmt.Println(ptr.FirstName)
+	fmt.Println(p.Age)
 	ptr.FirstName = "lucas"
+	increamentAge(ptr)
 	fmt.Printf(p.FirstName)
+	fmt.Println(p.Age)
 }
