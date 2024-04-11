d0bc81882248edf689abc47dea76c66758e301ac
Psyon <sayan.h2020@gmail.com>
2024-04-11 15:30:42 +0330

    edit wrap

--- error/03-packages/01/main.go
+++ error/03-packages/01/main.go
@@ -14,4 +14,7 @@ func main() {
 	wrappedErr1 := fmt.Errorf("Error 1: %w", rootErr)
 	wrappedErr2 := fmt.Errorf("Error 2: %w", wrappedErr1)
 	fmt.Printf("wrappedErr2: %v\n", wrappedErr2)
+	fmt.Printf("wrappedErr2: %v\n", wrappedErr2)
+	fmt.Printf("Unwrap(wrappedErr2): %v\n", errors.Unwrap(wrappedErr2))
+	fmt.Printf("Unwrap(Unwrap(wrappedErr2)): %v\n", errors.Unwrap(errors.Unwrap(wrappedErr2)))
 }
