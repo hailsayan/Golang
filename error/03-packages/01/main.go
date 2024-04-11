2ccd99e38d1b4c1137ef3a885014e4332b81928e
Psyon <sayan.h2020@gmail.com>
2024-04-11 15:24:26 +0330

    add wrap

---
+++ error/03-packages/01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// err := errors.New("Error Message")
+
+func main() {
+	// Create a root error
+	rootErr := errors.New("Root error")
+	// Wrap the root error with additional context
+	wrappedErr1 := fmt.Errorf("Error 1: %w", rootErr)
+	wrappedErr2 := fmt.Errorf("Error 2: %w", wrappedErr1)
+	fmt.Printf("wrappedErr2: %v\n", wrappedErr2)
+}
