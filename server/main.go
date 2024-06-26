faecc6d3e4b9abbad0f159f294b51965d1539869
Psyon <sayan.h2020@gmail.com>
2024-06-26 22:24:41 +0330

    add product

--- server/main.go
+++ server/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/hello", handleHelloWorld)
+	http.HandleFunc("/getProduct/", getProduct)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -24,4 +27,15 @@ func handleHelloWorld(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonResponse)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 3 {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	productID := parts[2]
+	fmt.Fprintf(w, "Product ID: %s\n", productID)
 }
